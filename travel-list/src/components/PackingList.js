abaef916da6a2534223f4c26bddc02832c73cbb2
Anurag-Yadav03 <03yadavanurag@gmail.com>
2023-06-15 23:41:46 +0530

    added eat-n-split

--- travel-list/src/components/PackingList.js
+++ travel-list/src/components/PackingList.js
@@ -1,39 +1,49 @@
-import {useState} from "react";
+import { useState } from "react";
 import Item from "./Item";
 
 export default function PackingList({
-    items,
-    onDeleteItem,
-    onToggleItem,
-    onClearlist,
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearList,
 }) {
-    const  [sortBy,setSortBy]  = useState("input");
-    let sortedItems;
+  const [sortBy, setSortBy] = useState("input");
 
-    if(sortBy === "input") sortedItems = items;
+  let sortedItems;
 
-    if(sortBy === "description")
-    sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+  if (sortBy === "input") sortedItems = items;
 
-    if(sortBy === "packed") 
-    sortedItems= items.slice().sort((a,b) => a.packed-Number(b.packed));
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
 
-    return (
-        <div className="list">
-            <ul>
-                {sortedItems.map((item) => (
-                    <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id}/>
-                ))}
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
 
-                <div className="actions">
-                    <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-                        <option value="input">Sort by input order</option>
-                        <option value="description">Sort by description</option>
-                        <option value="packed"> Sort by Packed status </option>
-                    </select>
-                    <button onClick={onClearlist}>Clear list</button>
-                </div>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
+        ))}
+      </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear list</button>
+      </div>
+    </div>
+  );
 }
